04b9979cda5776064f315852f33a523355ac16d6
Mikhel <m282021@gmail.com>
2018-09-30 22:16:40 +0300

    w2 failed memory test

--- src/com/mikekekeke/week2/WeekTwoRunner.java
+++ src/com/mikekekeke/week2/WeekTwoRunner.java
@@ -7,10 +7,9 @@ import java.util.EmptyStackException;
 
 public class WeekTwoRunner {
     public static void main(String[] args) {
-        for (int i = 0; i < 50; i++) {
-            int rnd = StdRandom.uniform(0, 2);
-            if (rnd == 2) throw new IllegalStateException();
-            StdOut.println(rnd);
-        }
+        int[] a1 = {1,2,3,4};
+        int[] a2 = a1;
+        a1[2] = -100;
+        StdOut.println(a2[2]);
     }
 }
--- src/com/mikekekeke/week2/assignment/RandomizedQueue.java
+++ src/com/mikekekeke/week2/assignment/RandomizedQueue.java
@@ -7,6 +7,31 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Test 8: Insert T items into queue; then iterate over queue and check
+ *         that worst-case constant memory is allocated or deallocated
+ *         per iterator operation.
+ *   * T = 64
+ *   * T = 128
+ *     - failed on trial 96 of 128
+ *     - when the randomized queue contains 128 objects,
+ *     - with 33 objects remaining to be iterated over;
+ *     - the call to next() caused a change in memory of -512 bytes
+ *     - any change of more than 480 bytes fails the test
+ *
+ *   * T = 256
+ *     - failed on trial 192 of 256
+ *     - when the randomized queue contains 256 objects,
+ *     - with 65 objects remaining to be iterated over;
+ *     - the call to next() caused a change in memory of -1024 bytes
+ *     - any change of more than 480 bytes fails the test
+ *
+ * ==> FAILED
+ * Seems like memory change caused by array shrinking.
+ * Maybe do another implementation of iterator,
+ * where possible to iterate over preallocated randomized array
+ */
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private int n;
     private Item[] data;
